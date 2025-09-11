7be33984826f81132c118ee48390449edfa9668e
ldastiUMD <ldasti@terpmail.umd.edu>
2025-09-11 18:03:58 -0400

    Added HTML page to app.py

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Helloooo World from Robert Smith!'
+    return 'Hellooo World from Robert Smith!'
 
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run()
