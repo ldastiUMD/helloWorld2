47f40526f287a1b97d0f2b616e134fbe977d93dc
ldastiUMD <ldasti@terpmail.umd.edu>
2025-10-31 19:09:46 -0400

    Changed db.string(100) to VARCHAR

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import VARCHAR  # <-- Added import for VARCHAR
+
 db = SQLAlchemy()
+
 
 class Student(db.Model):
     __tablename__ = "student"
@@ -7,7 +10,7 @@ class Student(db.Model):
     student_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), nullable=False)   # NEW FIELD — VARCHAR(100), REQUIRED
+    email = db.Column(VARCHAR(100), nullable=False)
     major_id = db.Column(db.Integer, db.ForeignKey('major.major_id'))
     birth_date = db.Column(db.DateTime, nullable=False)
     num_credits_completed = db.Column(db.Integer, nullable=False)
